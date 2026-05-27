2739a348b76c50eee32467cabaedacd79d2b958f
Seif Mohamed <20p1710@eng.asu.edu.eg>
2026-05-27 21:13:14 +0300

    add(auth): add login screen

---
+++ src/features/auth/LoginPage.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useAuth } from "../../context/AuthContext";
+import { Button, Card, Input } from "../../components/ui";
+
+type CredentialsType = {
+  email: string;
+  password: string;
+};
+
+export default function LoginPage() {
+  const [credentials, setCredentials] = useState<CredentialsType>({
+    email: "",
+    password: "",
+  });
+  const { login } = useAuth();
+
+  function handleCredentialsInput(e: React.ChangeEvent<HTMLInputElement>) {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e: React.SyntheticEvent<HTMLFormElement>) {
+    e.preventDefault();
+    login(credentials?.email, credentials?.password);
+  }
+
+  return (
+    <div className="bg-gray-50 min-h-screen grid place-items-center">
+      <Card className="w-full max-w-md ">
+        <h2 className="text-center font-bold text-2xl mb-8">Welcome Back</h2>
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div>
+            <Input
+              label="Email"
+              type="text"
+              name="email"
+              onChange={handleCredentialsInput}
+              value={credentials?.email}
+            />
+          </div>
+          <div>
+            <Input
+              label="Password"
+              type="password"
+              name="password"
+              onChange={handleCredentialsInput}
+              value={credentials?.password}
+            />
+          </div>
+          <Button className="w-full">Log in</Button>
+        </form>
+      </Card>
+    </div>
+  );
+}
