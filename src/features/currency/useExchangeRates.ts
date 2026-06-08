32fffcf00578754af3f1e8242da5b208fa17118b
Seif Mohamed <20p1710@eng.asu.edu.eg>
2026-06-08 04:25:12 +0300

    refactor(currency): add tanstack query in useExchangeRates instead of manual fetch

--- src/features/currency/useExchangeRates.ts
+++ src/features/currency/useExchangeRates.ts
@@ -1,23 +1,23 @@
-import { useEffect, useState } from "react";
 import type { ExchangeRate, FetchState } from "../../types";
 import { fetchExchangeRate } from "../../services/currencyService";
+import { useQuery } from "@tanstack/react-query";
 
 export default function useExchangeRates(): FetchState<ExchangeRate> {
-  const [rate, setRate] = useState<FetchState<ExchangeRate>>({
-    type: "loading",
+  const {
+    data: ExchangeRates,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["ExchangeRates"],
+    queryFn: fetchExchangeRate,
   });
-  useEffect(() => {
-    async function load() {
-      const result = await fetchExchangeRate();
-      if (result.type === "success") {
-        setRate({ type: "success", data: result.data });
-      } else {
-        setRate({ type: "error", error: result.error });
-      }
-    }
-
-    load();
-  }, []);
-
-  return rate;
+  if (isLoading) return { type: "loading" };
+  if (isError)
+    return { type: "error", error: error?.message ?? "Failed to fetch" };
+  if (ExchangeRates?.type === "success") {
+    return { type: "success", data: ExchangeRates.data };
+  } else {
+    return { type: "error", error: ExchangeRates?.error ?? "Unknown Error" };
+  }
 }
