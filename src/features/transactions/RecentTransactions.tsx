c6af1f9c6cc253c200b49b71dad58e58b99a5bee
Seif Mohamed <20p1710@eng.asu.edu.eg>
2026-06-15 18:06:39 +0300

    refactor(transactions): add transaction-item

--- src/features/transactions/RecentTransactions.tsx
+++ src/features/transactions/RecentTransactions.tsx
@@ -1,8 +1,5 @@
-import { format } from "date-fns";
 import type { Account, Transaction } from "../../types";
-import { Badge } from "../../components/ui";
-import { formatCurrency } from "../../utils";
-import { transactionTypeBadge } from "./transactionUtils";
+import TransactionItem from "./TransactionItem";
 
 type RecentTransactionsProps = {
   transactions: Transaction[];
@@ -16,36 +13,16 @@ export default function RecentTransactions({
   return (
     <div>
       <p className="text-xl font-semibold">Recent Transactions</p>
-      {transactions.slice(0, 5).map((transaction) => {
-        return (
-          <div
-            className=" border-gray-400 border-b flex flex-col py-4"
-            key={transaction.id}
-          >
-            <Badge
-              variant={transactionTypeBadge[transaction.type]}
-              className="self-end"
-            >
-              {transaction.type}
-            </Badge>
-            <p className="text-gray-500 text-sm">
-              {
-                accounts.find((account) => account.id === transaction.accountId)
-                  ?.name
-              }
-            </p>
-
-            <p className="font-semibold text-lg">
-              {formatCurrency(transaction.amount)}
-            </p>
-            <p>Balance After: {formatCurrency(transaction.balanceAfter)}</p>
-            <div className="flex justify-between text-sm text-gray-400">
-              <p>{transaction.description}</p>
-              <p>{format(transaction.date, "MM/dd/yyyy")}</p>
-            </div>
-          </div>
-        );
-      })}
+      {transactions.slice(0, 5).map((transaction) => (
+        <TransactionItem
+          key={transaction.id}
+          transaction={transaction}
+          accountName={
+            accounts.find((account) => account.id === transaction.accountId)
+              ?.name
+          }
+        />
+      ))}
     </div>
   );
 }
